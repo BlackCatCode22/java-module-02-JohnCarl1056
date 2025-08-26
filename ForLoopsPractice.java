c195a3170f71a235f40badeeaf3f3efcb3cd98e0
JohnCarl1056 <jcgonzalez0803@yahoo.com>
2025-08-26 11:42:10 -0700

    Add files via upload

---
+++ ForLoopsPractice.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class ForLoopsPractice {
+    public static void main(String[] args) {
+
+        // A loop must have a loop
+        // Control a variable
+        // The loop control variable must
+        // be initialized, checked, and changed
+        for(int i = 1; i <= 1000; i++) {
+            System.out.println(i);
+
+        }
+    }
+}
