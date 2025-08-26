273082d7eec158ad929a299cde4863e8fd1d4e84
JohnCarl1056 <jcgonzalez0803@yahoo.com>
2025-08-26 11:16:14 -0700

    Add files via upload

---
+++ StringObjects.java
@@ -0,0 +1,10 @@
+//jg 08/26/25
+public class StringObjects { public static void main(String[] args) {
+        String phrase = "Giraffe Academy";
+        System.out.println(phrase.toUpperCase());
+System.out.println(phrase.toLowerCase());
+System.out.println(phrase.charAt(1) );
+System.out.println(phrase.indexOf("affe") );
+System.out.println(phrase.substring(0,3));
+}
+}
