c41358b3fd09cfb9eec1ec96d35a50f70d40eb51
JohnCarl1056 <jcgonzalez0803@yahoo.com>
2025-09-02 11:49:57 -0700

    Add files via upload

---
+++ MadnessWithMethodsNumbers.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class MadnessWithMethodsNumbers {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+
+        int sum = add(num1, num2);
+        System.out.println(sum);
+    }
+    static int add (int num1, int num2) {
+
+        return num1 + num2;
+
+    }
+}
